6865116e3bda852e21bde238c8999ecb412c9fda
Niicklz <77895883+Niicklz@users.noreply.github.com>
2024-02-09 12:35:06 -0400

    fix videourl

--- src/data/videosData.js
+++ src/data/videosData.js
@@ -1,37 +1,37 @@
 export const videosData = [
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
-    videoUrl: "/public/example-vid.mp4",
+    videoUrl: "../../public/example-vid.mp4",
     poster:
       "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
-    videoUrl: "/public/example-vid.mp4",
+    videoUrl: "../../public/example-vid.mp4",
     poster:
       "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
-    videoUrl: "/public/example-vid.mp4",
+    videoUrl: "../../public/example-vid.mp4",
     poster:
       "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
-    videoUrl: "/public/example-vid.mp4",
+    videoUrl: "../../public/example-vid.mp4",
     poster:
       "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
-    videoUrl: "/public/example-vid.mp4",
+    videoUrl: "../../public/example-vid.mp4",
     poster:
       "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
-    videoUrl: "/public/example-vid.mp4",
+    videoUrl: "../../public/example-vid.mp4",
     poster:
       "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
