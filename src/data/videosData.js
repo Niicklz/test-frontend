162257d76c6f74e51e84d56581baef9acd2dc528
Niicklz <77895883+Niicklz@users.noreply.github.com>
2024-02-09 13:07:16 -0400

    removing posters

--- src/data/videosData.js
+++ src/data/videosData.js
@@ -3,37 +3,25 @@ export const videosData = [
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
     videoUrl: video,
-    poster:
-      "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
     videoUrl: video,
-    poster:
-      "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
     videoUrl: video,
-    poster:
-      "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
     videoUrl: video,
-    poster:
-      "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
     videoUrl: video,
-    poster:
-      "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
   {
     title: "Lorem ipsum dolor sit amet consectetur.",
     videoUrl: video,
-    poster:
-      "https://res.cloudinary.com/dooazvgi7/image/upload/v1707445709/randomvideo/500x500_m3b71s.jpg",
   },
 ];
