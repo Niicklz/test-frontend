e9d9e545a339a648b4c33568ff97ff89766d69b6
Niicklz <77895883+Niicklz@users.noreply.github.com>
2024-02-09 13:01:48 -0400

    fix video

--- src/components/VideoSection/VideoCard/VideoCard.jsx
+++ src/components/VideoSection/VideoCard/VideoCard.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./VideoCard.css";
 
 import { Heading } from "../../commons/Heading/Heading";
+import video from "../../../../public/video.mp4";
 
 export const VideoCard = ({ videoUrl, title, poster }) => {
   return (
     <article className="video-card">
       <video
-        width="320px"
+        width="320"
         height="150"
         id="mainVideo"
-        src="/public/example-vid.mp4"
+        src={video}
         muted
         controls
       ></video>
